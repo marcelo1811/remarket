7be56e04f4aaed6f5d62e48a2a874c8f46fa02ec
marcelo1811 <marcelomiyachi@MacBook-Air.local>
2020-08-08 13:20:41 -0300

    feat: add price to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,7 +38,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :is_active)
+    params.require(:product).permit(:name, :description, :is_active, :price)
   end
 
   def set_store
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_014433) do
+ActiveRecord::Schema.define(version: 2020_08_08_161904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_08_07_014433) do
     t.boolean "is_active", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "price"
     t.index ["store_id"], name: "index_products_on_store_id"
   end
 
