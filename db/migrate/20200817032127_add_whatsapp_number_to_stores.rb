a8095751497f06596a47cca7d402561d6760728d
marcelo1811 <marcelomiyachi@MacBook-Air.local>
2020-08-18 00:38:27 -0300

    fix: migration s

--- db/migrate/20200817032127_add_whatsapp_number_to_stores.rb
+++ db/migrate/20200817032127_add_whatsapp_number_to_stores.rb
@@ -1,4 +1,4 @@
-class Addwhatsapp_numberNumberToStores < ActiveRecord::Migration[6.0]
+class AddWhatsappNumberNumberToStores < ActiveRecord::Migration[6.0]
   def change
     add_column :stores, :whatsapp_number, :string
   end
--- db/migrate/20200817032135_add_whatsapp_number_to_catalogues.rb
+++ db/migrate/20200817032135_add_whatsapp_number_to_catalogues.rb
@@ -1,4 +1,4 @@
-class Addwhatsapp_numberNumberToCatalogues < ActiveRecord::Migration[6.0]
+class AddWhatsappNumberNumberToCatalogues < ActiveRecord::Migration[6.0]
   def change
     add_column :catalogues, :whatsapp_number, :string
   end
